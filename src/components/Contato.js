3c7846e094ed76163e709465effd451227717489
Joao Manoel <joao.manoel@edu3.com.br>
2024-10-31 21:34:37 -0300

    estruturacao do portfolio e banner feito

---
+++ src/components/Contato.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contato = () => {
+  return (
+    <div className="section" id="contato">
+      Contato
+    </div>
+  );
+};
+
+export default Contato;
---
+++ src/components/Experiencia.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Experiencia = () => {
+  return (
+    <div className="section" id="experiencia">
+      Experiência
+    </div>
+  );
+};
+
+export default Experiencia;
